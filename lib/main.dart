a05d4fed0465e00b2bad298eef1efa795ffd2bd6
danielkachi <danielhenrykachi@yahoo.com>
2024-08-10 14:25:53 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyMapPage(),
+    );
+  }
+}
+
+class MyMapPage extends StatefulWidget {
+  const MyMapPage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyMapPageState createState() => _MyMapPageState();
+}
+
+class _MyMapPageState extends State<MyMapPage> {
+  late final GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Map Example'),
+      ),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 11.0,
+        ),
+      ),
+    );
+  }
+}
